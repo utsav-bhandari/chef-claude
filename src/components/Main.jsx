af9277c79864e7cabefbd1a78e89ece6ca976ab8
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2025-05-09 00:08:02 -0700

    added form with styling

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    />
+                <button>
+                    Add ingredient
+                </button>
+            </form>
+        </main>
+    )
+}
