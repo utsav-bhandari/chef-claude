ed5a90e382184150a5fe151db37e82fc05fe45b9
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2025-05-09 01:38:37 -0700

    can add form item to list now

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 export default function Main() {
+    let [ingredients, setIngredients] = useState([]);
+    let ingredientsList = ingredients.map(i => <li key={i}>{i}</li>);
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredientsList => [...prevIngredientsList, newIngredient])
+        console.log(ingredientsList)
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input
                     type="text"
                     placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
                     />
                 <button>
                     Add ingredient
                 </button>
             </form>
+            <ul>
+                {ingredientsList}
+            </ul>
         </main>
     )
 }
